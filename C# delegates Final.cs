aa4b8530f143e73665fb4528a55ffdcc2f27e0ed
chetanpal14 <137135302+chetanpal14@users.noreply.github.com>
2023-08-18 09:15:20 -0400

    Add files via upload

---
+++ C# delegates Final.cs
@@ -0,0 +1,29 @@
+﻿delegate void MathOperation(int x, int y);
+
+class Program
+{
+    public static void Add(int a, int b)
+    {
+
+        Console.WriteLine($"Addition result {a} + {b} = " + (a +b));
+
+    }
+    public static void subtract(int a, int b)
+    {
+
+        Console.WriteLine($"Addition result {a} - {b} = " + (a - b));
+    }
+
+    static void Main(string[] args)
+    {
+       
+        MathOperation calc = new MathOperation(Add);
+
+        calc += subtract;
+       
+        calc(40, 30);
+
+
+
+    }
+}
